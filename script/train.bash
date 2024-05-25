ab1d9229aa3f6283e097f830fec8b2eb283f953c
wzh <as456741@qq.com>
2024-05-25 19:57:29 +0800

    add continue param in the train script

--- script/train.bash
+++ script/train.bash
@@ -3,5 +3,6 @@
 cd ..
 n_j=$1
 n_m=$2
-python train_ppo.py --n_j="${n_j}" --n_m="${n_m}"
+continue_model_dir=$3
+python train_ppo.py --n_j="${n_j}" --n_m="${n_m}" --continue_model_dir="${continue_model_dir}"
 echo "${n_j}*${n_m} model train loop is over!"
