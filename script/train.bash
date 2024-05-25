b28631e2d1b7d5cba018519a0dcbec0bdf892e4b
wzh <as456741@qq.com>
2024-05-25 20:24:02 +0800

    bug fix

--- script/train.bash
+++ script/train.bash
@@ -2,13 +2,13 @@
 cd ..
 n_j=$1
 n_m=$2
-continue_model_dir=$3
+continue_model_path=$3
 
-if [ -z "${continue_model_dir}" ]; then
+if [ -z "${continue_model_path}" ]; then
     # continue_model_dir为空或未设定
     python train_ppo.py --n_j "${n_j}" --n_m "${n_m}"
 else
     # continue_model_dir已设定
-    python train_ppo.py --n_j "${n_j}" --n_m "${n_m}" --continue_model_dir "${continue_model_dir}"
+    python train_ppo.py --n_j "${n_j}" --n_m "${n_m}" --continue_model_path "${continue_model_path}"
 fi
 echo "${n_j}*${n_m} model train loop is over!"
