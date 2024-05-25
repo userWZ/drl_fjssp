b78d3ef90a2f93742c36af3509ececd42a7050f7
wzh <as456741@qq.com>
2024-05-25 20:02:54 +0800

    update train.bash

--- script/train.bash
+++ script/train.bash
@@ -4,5 +4,8 @@ cd ..
 n_j=$1
 n_m=$2
 continue_model_dir=$3
+if continue_model_dir is None; then
+    continue_model_dir=None
+fi
 python train_ppo.py --n_j="${n_j}" --n_m="${n_m}" --continue_model_dir="${continue_model_dir}"
 echo "${n_j}*${n_m} model train loop is over!"
